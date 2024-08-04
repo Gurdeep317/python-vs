f1e49308dbe4fb55b65854d627935165885e8c1e
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-04 14:45:49 +0000

    first commit

---
+++ while.py
@@ -0,0 +1,17 @@
+s=0
+while s<=10:
+  print("gurdeep")
+  s=s+1
+print(s)
+print()
+i=10
+while i>=1:
+  print("hello")
+  i=i-1
+print(i)
+print()
+h="welcome"
+print(h[3])
+print(h[0: :2])
+print(h[-1: :-1])
+print(h[-1:-8 :-2])
