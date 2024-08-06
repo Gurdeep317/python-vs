b3dd13740838b44825ea42e003bca223455e0320
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-06 17:12:07 +0000

    first commit

---
+++ quadratic.py
@@ -0,0 +1,8 @@
+import cmath
+a=int(input("enter the value of a="))
+b=int(input("enter the value of b="))
+c=int(input("enter the value of c="))
+d=(b*b) - (4*a*c)
+sol1=(-b + cmath.sqrt(d)) / (2*a)
+sol2=(-b - cmath.sqrt(d)) / (2*a)
+print("the given roots are:",sol1 ,sol2)
