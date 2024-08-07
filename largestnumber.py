11959a9847737bd169736566e99859b53783027e
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-07 14:19:58 +0000

    first commit

---
+++ largestnumber.py
@@ -0,0 +1,9 @@
+num1=float(input("enter the num1="))
+num2=float(input("enter the num2="))
+num3=float(input("enter the num3="))
+if (num1>num2) and (num1>num3):
+    print("num1 is a largest number")
+elif (num2>num3) and (num2>num1):
+    print("num2 is a largest number")
+else:
+    print("num3 is a largest number")
