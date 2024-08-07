0d177fd50097a9aff1ce6bfb247d1e8c090fdcea
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-08-07 18:58:53 +0530

    first commit

---
+++ positive.py
@@ -0,0 +1,7 @@
+num=float(input("enter the value of num="))
+if num>0:
+    print("num1 is a positive number")
+elif num<0:
+    print("num1 is a negative number")
+else:
+    print("number is zero")
