a25816d2cbfaec68f131155e2e08d8bcb0fcd6c1
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-08 12:09:38 +0000

    first commit

---
+++ primeinterval.py
@@ -0,0 +1,10 @@
+lower=int(input("enter the value of lower="))
+upper=int(input("enter the value of upper="))
+
+for i in range (lower,upper+1):
+    if i>1:
+        for num in range(2,i):
+           if i%num == 0:
+               break
+        else:
+         print(i)
