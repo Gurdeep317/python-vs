e2f6f499554dea28ef1cb8906f9d4ebd22832002
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-09 19:21:29 +0000

    first commit

--- multiplication.py
+++ multiplication.py
@@ -1,3 +1,7 @@
+# multiplication table in python
+# taking input from the user
 num=int(input("enter a no for table="))
+# using for loop
+# iterate 10 times from i = 1 to 10
 for i in range (1,11):
     print(num,"*",i,"=",num*i)
