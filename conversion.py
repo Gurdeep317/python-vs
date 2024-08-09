3493edc37018bb3825dd45e07c37526b81621984
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-09 19:41:55 +0000

    first commit

--- conversion.py
+++ conversion.py
@@ -1,3 +1,5 @@
+# python program for conversion of decimal into binary,octal,hexadecimal 
+# taking a input of decimal value from user
 dec=int(input("enter a decimal number="))
 print("the decimal value of",dec,"is:")
 print(bin(dec),"in binary")
