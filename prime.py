0ac451550f9f95d533fd41bfe13553dcb6aaa9d0
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-09 08:16:09 +0000

    first commit

--- prime.py
+++ prime.py
@@ -1,13 +1,12 @@
 num=int(input("enter a number="))
-# if num==1:
-#     print("number is not prime")
- if num>1:
-        for i in range (2,num):
-            if num % i == 0:
-                print("number is not a prime number ")
-                break
-             else :
-         print("number is a prime number") 
-else:
-    print("number is not a prime number")
-       
+if num == 1:
+    print("no is not a prime number")
+if num>1:
+    for i in range(2,num):
+        if num% i == 0:
+            print("no is not a prime number")
+            break
+
+        else:
+            print("no is a prime number")
+            break
