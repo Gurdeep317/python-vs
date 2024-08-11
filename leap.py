baf42a1ae46baa289a354ae960551cecd788ba5b
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-11 18:28:44 +0000

    first commit

--- leap.py
+++ leap.py
@@ -1,3 +1,7 @@
+# taking input from the user
+# use if ,elif,else condition 
+# leap year which is divisible by 400 and 100
+# also leap year which is divisible by 4 but not by 100 
 year=int(input("enter a year="))
 if (year % 400 == 0) and (year % 100 == 0):
     print("year is a leap year")
