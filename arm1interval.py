f443c8ee9e23edb014678bccd491d45763a09cf0
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-11 18:23:23 +0000

    first commit

--- arm1interval.py
+++ arm1interval.py
@@ -1,12 +1,12 @@
 lower=int(input("enter a lower limit:"))
 upper=int(input("enter a upper limit:"))
 for num in range(lower,upper+1):
+    order=len(str(num))
     sum=0
     temp=num
     while temp>0:
        digit=temp%10
-       cube=digit**3
-       sum=sum+cube
+       sum+=digit**order
        temp//=10
-    if num==sum:
+if num==sum:
        print(num)
