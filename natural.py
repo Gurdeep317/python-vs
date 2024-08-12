663b5359849ce61ea89be541a467f59be24a820b
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-12 17:40:09 +0000

    first commit

--- natural.py
+++ natural.py
@@ -1,3 +1,8 @@
+# taking input from user
+# use if ,else condition and while loop
+# if num is greater than zero than using while loop 
+# initially taking sum is zero than add num into sum
+# and then decreament of 1 from number 
 num=int(input("enter a number:"))
 if num<0:
     print("enter a positive number")
