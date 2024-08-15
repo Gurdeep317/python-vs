ed91718a2a05c9a6a512b43f091547c2983c6642
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-15 18:17:18 +0000

    first commit

--- prime.py
+++ prime.py
@@ -1,3 +1,7 @@
+# taking input from user 
+# using if statement and for loop 
+# if num is 1 so that is not a prime number
+# if num is greater than one than check it using for loop
 num=int(input("enter a number="))
 if num == 1:
     print("no is not a prime number")
