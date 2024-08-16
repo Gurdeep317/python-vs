a8e6ca733eaa23f8dc77d52eeb9edf32ff44f5d2
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-08-16 23:44:08 +0530

    firts commit

--- lcm.py
+++ lcm.py
@@ -1,3 +1,6 @@
+# taking two numbers from user
+# find which is greater
+# if greater number is divisible by both numbers and remainder is zero than greater number is the lcm of two numbers
 a=int(input("enter a number:"))
 b=int(input("enter a another number:"))
 maxnum=max(a,b)
