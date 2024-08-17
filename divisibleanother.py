099345f4e98729bbaee288175aa6b6e26684b1ce
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-08-17 23:10:56 +0530

    first commit

--- divisibleanother.py
+++ divisibleanother.py
@@ -7,6 +7,9 @@ for i in range(1,100):
         print(i)
 
 print()
+# second solution of this program
+# here we use list ,filter and lambda function 
+# lambda function is a temporary and anonymous function
 l=[66,39,48,98,26,87,67]
 result=list(filter(lambda x:x%13==0,l))
 print("the numbers divisible by 13 is:",result)
