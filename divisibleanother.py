f3371462fc93dd41d6eaf693188137383b19b89d
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-08-17 23:03:38 +0530

    first commit

--- divisibleanother.py
+++ divisibleanother.py
@@ -2,3 +2,8 @@ print("the number is divisible by 13 is:")
 for i in range(1,100):
     if i%13==0:
         print(i)
+
+print()
+l=[66,39,48,98,26,87,67]
+result=list(filter(lambda x:x%13==0,l))
+print("the numbers divisible by 13 is:",result)
