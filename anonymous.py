697e2a2c51438caf62b783dc08f1a34d097056bd
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-08-17 22:35:03 +0530

    first commit

--- anonymous.py
+++ anonymous.py
@@ -1,3 +1,6 @@
+# program using anonymous function 
+# we use here map function ,lambda and for loop
+# taking input from user
 nterms=int(input("enter number of terms:"))
 result=list(map(lambda x:2**x,range(nterms+1)))
 for i in range(nterms+1):
