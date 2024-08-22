3253709807167b5d4a28d3d2339c7e0a50eb8261
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-22 19:44:53 +0000

    first commit

--- decimalrec.py
+++ decimalrec.py
@@ -1,3 +1,9 @@
+# program to convert decimal to binary using recursion
+# using def function
+# taking n as a input from user 
+# using floor division
+# using modulus for remainder
+# using if condition
 def convertdecimal(n):
     if n>1:
         convertdecimal(n//2)
