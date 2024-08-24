4b4a60cfca0b0d9b1bddd283038b8374d3081989
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-08-24 22:25:25 +0530

    first commit

---
+++ multiplymatrices.py
@@ -0,0 +1,15 @@
+A=[[1,3,5],
+   [2,4,6],
+   [7,8,9]]
+B=[[1,5,3],
+   [4,7,3],
+   [0,2,3]]
+result=[[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+for i in range (len(A)):
+    for j in range(len(A[0])):
+        result[i][j]=A[i][j]*B[i][j]
+for i in result :
+    print(i)
+
