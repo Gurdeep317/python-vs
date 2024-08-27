e1e16fd61d034e26f3a00a68bce36d9552efa577
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-27 17:51:03 +0000

    first commit

--- rightangle.py
+++ rightangle.py
@@ -1,3 +1,7 @@
+# program to create right angle triangle patterns
+# taking number of rows from user as a input
+# using a for loop for rows
+# use another for loop for columns
 num=int(input("enter number of rows:"))
 for i in range(1,num+1):
     for j in range(1,i+1):
