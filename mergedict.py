fe4afc096719311ab52d9fda824580dde301659c
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-08-28 22:43:03 +0530

    first commit

--- mergedict.py
+++ mergedict.py
@@ -1,6 +1,9 @@
 # program to merge two dictionaries
 # taking two dictionaries
 # merge using bar operator
+# dict1={"hello":34,"welcome":76}
+# dict2={"welcome":45,"rao":34}
+# print(dict1|dict2)
 dict1={"hello":34,"welcome":76}
 dict2={"welcome":45,"rao":34}
-print(dict1|dict2)
+print({**dict1,**dict2})
