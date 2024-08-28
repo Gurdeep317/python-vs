63ed9f7cc78094d645df38ef06180aa0a3eaf1ea
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-08-28 22:51:14 +0530

    first commit

--- mergedict.py
+++ mergedict.py
@@ -4,6 +4,8 @@
 # dict1={"hello":34,"welcome":76}
 # dict2={"welcome":45,"rao":34}
 # print(dict1|dict2)
+# another solution to merge two dictionaries
+# using kwargs operator
 dict1={"hello":34,"welcome":76}
 dict2={"welcome":45,"rao":34}
 print({**dict1,**dict2})
