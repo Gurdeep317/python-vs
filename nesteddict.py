b869dfb47220974a7a581f30cca864eec9d85b3f
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-08-29 23:21:03 +0530

    first commit

--- nesteddict.py
+++ nesteddict.py
@@ -1,2 +1,6 @@
+# program to create nested directory
+# import path from pathlib module
+# enter location where you want to create folder
+# mkdir used to make directory
 from pathlib import Path
 Path("C:/Users/lenovo/PycharmProjects/pythonProject/new dict/demo_dict").mkdir(parents=True,exist_ok=True)
