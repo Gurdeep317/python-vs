29a88c7060d783e98acdda1026b6b4a815f62e12
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-08-30 23:22:45 +0530

    first commit

--- iterate.py
+++ iterate.py
@@ -1,3 +1,6 @@
+# profram to  Iterate Over Dictionaries Using for Loop
+# using for loop
+# taking list
 l={"name":"radha","age" : "24","fee" : "300"}
 for key,value in l.items():
     print(key,value)
