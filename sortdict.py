6dc39d2884dda41e2247d14abb9d15255dc396a1
Gurdeep <gurdeepdalal05@gmail.com>
2024-08-31 18:25:06 +0000

    first commit

--- sortdict.py
+++ sortdict.py
@@ -1,6 +1,7 @@
 # Program to Sort a Dictionary by Value 
 # taking a dictionary and then sort it
 # taking a temporary function lambda
+# solution 1 sort the dictionary based on value
 marks={"meena":43,"riya":35,"annu":87}
 print(marks)
 sorted=sorted(marks.items(),key=lambda x:x[1])
