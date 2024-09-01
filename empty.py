f2c39fedc88bf31b68f82e1f8ad74c8a50165556
Gurdeep <gurdeepdalal05@gmail.com>
2024-09-01 18:06:33 +0000

    first commit

--- empty.py
+++ empty.py
@@ -3,5 +3,10 @@
 # taking a list
 # here not work as boolean operator
 list=[]
-if not list:
-    print("the list is empty")
+# if not list:
+#     print("the list is empty")
+
+
+print(list)
+if len(list)==0:
+    print("list is empty")
