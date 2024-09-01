747e451ad4ce38c9b60fdd9c801bb7862a6a8875
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-09-01 23:32:22 +0530

    first commit

---
+++ sqll.py
@@ -0,0 +1,9 @@
+import sqlite3
+conn=sqlite3.connect("sqlite.db")
+ins='''
+      insert into student(st_name,st_class,st_email)
+      VALUES('ram','12th','ravi@gmail.com')
+      '''
+conn.execute(ins)
+conn.commit()
+conn.close()
