61bfbce8ef0c6bb9c429380f8f62373ce6d55841
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-09-04 23:58:13 +0530

    first commit = first month commits

--- keypresent.py
+++ keypresent.py
@@ -1,3 +1,6 @@
+# Python Program to Check if a Key is Already Present in a Dictionary
+# taking a dictionary
+# using if condition
 dict={"name":"hello",1:"a",2:"b"}
 # user=input("enter any key :")
 print(dict)
