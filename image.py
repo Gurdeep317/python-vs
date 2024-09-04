df155ca0f3b6be93c5578e56ce6922a98b05ed05
Gurdeep <gurdeepdalal05@gmail.com>
2024-09-04 18:20:35 +0000

    first commit = first month commits

--- image.py
+++ image.py
@@ -1,8 +1,1 @@
-import PIL
-from PIL import Image
-def new_func():
-    return PIL.Image.open("C:/Users/lenovo/OneDrive/Desktop/ZZZZZZZ")
-
-img=new_func()
-width,height=img.size
-print(width ,"x",height)
+ 
