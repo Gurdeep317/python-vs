e813f8f6b3824859022509228631ef16aee6a527
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-09-15 15:18:04 +0530

    second commit

--- fullpath.py
+++ fullpath.py
@@ -1,3 +1,6 @@
+# program to get the full path of the current working directory
 import pathlib
+# path of given file
 print(pathlib.Path('leap.py').parent.absolute())
+# current working directory
 print(pathlib.Path().absolute())
