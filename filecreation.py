4771f465bd0cd4897b1743bd4dd685490e9f3c42
Gurdeep <gurdeepdalal05@gmail.com>
2024-09-15 10:02:05 +0000

    second commit

--- filecreation.py
+++ filecreation.py
@@ -1,3 +1,6 @@
+# program to get file creation and modification date
+# using os,pathlib and time module
+# using ctime,mtime
 import os,pathlib,time
 file=pathlib.Path("C:\\Users\\lenovo\\PycharmProjects\\pythonProject\\.venv")
 print("the creation date time:",time.ctime(os.path.getctime(file)))
