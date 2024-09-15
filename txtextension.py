5e75f3ed6b8c42d076af4b995b6af2cc9eb4da26
Gurdeep <gurdeepdalal05@gmail.com>
2024-09-15 10:33:42 +0000

    second commit

--- txtextension.py
+++ txtextension.py
@@ -1,3 +1,7 @@
+# program to find all file with .txt extension present inside a dictionary
+# using os module
+# using for loop
+# using if condition
 import os
 for file in os.listdir("C:\\Users\\lenovo\\OneDrive"):
    if (file.endswith(".txt")):
