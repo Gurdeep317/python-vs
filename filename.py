19724458750d7edb7474967120fb0ff1f149ce7a
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-09-19 22:35:08 +0530

    second commit

--- filename.py
+++ filename.py
@@ -1,3 +1,5 @@
+# Python Program to Get the File Name From the File Path
+# using os module
 import os
 filename=os.path.basename("C:\\Users\\lenovo\\PycharmProjects\\pythonProject\\.venv")
 print(filename)
