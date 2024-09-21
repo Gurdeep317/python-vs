58623adf5dd6f92762a3393c60826c67e7e6b02e
Gurdeep <gurdeepdalal05@gmail.com>
2024-09-21 19:38:05 +0000

    second commit

--- fileextension.py
+++ fileextension.py
@@ -1,7 +1,7 @@
 # Python Program to Extract Extension From the File Name
 # using os module
 import os
-file=os.path.splitext("C:\Users\lenovo\PycharmProjects\pythonProject/.venv")
+file=os.path.splitext("filename.py")
 print(file)
 print(file[0])
 print(file[1])
