c21f828dd3774981230d9ee6224b42ab2ed7d6f6
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-10-05 00:34:03 +0530

    third commit=third months commit

---
+++ trianglevolume.py
@@ -0,0 +1,8 @@
+# program to find the volume of triangle
+# taking input from user 
+# using formula of volume of triangle
+len=int(input("enter a no"))
+breadth=int(input('enter a no'))
+height=int(input("enter a no"))
+volume=(len*breadth*height)
+print("volume of triangle is:",volume)
