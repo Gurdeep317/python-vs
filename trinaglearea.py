037c4095cec2a06a9a26c8406fbc475499979326
Gurdeep <gurdeepdalal05@gmail.com>
2024-10-04 19:04:03 +0000

    third commit=third months commit

--- trinaglearea.py
+++
@@ -1,8 +0,0 @@
-# program to find the area of triangle
-# taking input from user 
-# using formula of area of triangle
-len=int(input("enter a no"))
-breadth=int(input('enter a no'))
-height=int(input("enter a no"))
-area=(len*breadth*height)/2
-print("area of triangle is:",area)
