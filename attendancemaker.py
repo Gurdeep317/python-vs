57fdf977b6bb8424fba1bbdee589d01f4d77a5cd
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-10-28 22:18:30 +0530

    Third Commit

--- attendancemaker.py
+++ attendancemaker.py
@@ -8,4 +8,36 @@ class AttendanceTracker:
     else:
       self.attendance=student_name
       print(f"Attendance marked for {student_name}.")
+
+    def  view_attendance(self):
+      print("attendance records:")
+      for student,count in self.attendance.items():
+        print(f"{student}:{count} times")
+def main():
+    tracker=AttendanceTracker() 
+    while True:
+        print("\n1. Mark attendance")     
+        print("2. View attendance")
+        print("3. Exit attendance")
+        choice=input("choose an option:")
+
+        if choice=="1":
+           name=input("enter student name:")
+           tracker.mark_attendance(name)
+        elif choice=="2":
+           tracker.view_attendance()
+        elif choice=="3":
+           print("exiting the program.")
+           break
+        else:
+           print("invalid choice,please try again.")
+if__name__=="__main__": 
+    main()
+
+
+
+    
+    
+     
+
       
