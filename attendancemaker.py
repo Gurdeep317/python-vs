9f455a12de9ba83dacfc8ae832482ac83e2114e6
Gurdeep317 <dalalgurdeep013@gmail.com>
2024-10-28 21:16:32 +0530

    Third Commit

---
+++ attendancemaker.py
@@ -0,0 +1,11 @@
+class AttendanceTracker:
+  def __init__(self):
+    self.attendance={}
+
+  def mark_attendance(self,student_name):
+    if student_name in self.attendance:
+      self.attendance=student_name+1
+    else:
+      self.attendance=student_name
+      print(f"Attendance marked for {student_name}.")
+      
