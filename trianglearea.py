4aba7db695b483f352367181c65ac9311e9a4c45
your name <dalalgurdeep013@gmail.com>
2024-11-17 23:43:04 +0530

    fourth commit

--- trianglearea.py
+++ trianglearea.py
@@ -5,6 +5,7 @@ height=int(input("enter the value of height:"))
 area=1/2*base*height
 print("area of triangle=",area)
 print()
+# another method
 base=float(input("enter the value of base:"))
 height=float(input("enter the value of height:"))
 area=1/2*base*height
