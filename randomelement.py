f1bd0e963afb555393850c48f0796109f13c7818
Gurdeep <gurdeepdalal05@gmail.com>
2024-11-23 17:40:13 +0000

    fourth commit

--- randomelement.py
+++ randomelement.py
@@ -1,5 +1,6 @@
 # Program to Randomly Select an Element From the List
 # using import module
-import random
+import random 
+# taking list
 list=[1,2,3,4,5]
-print(random.choice(list ))
+print(random.choice(list))
