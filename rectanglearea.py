b42c9e4690cff480aa26133729ca87d8593bb130
Gurdeep <gurdeepdalal05@gmail.com>
2024-11-25 17:55:04 +0000

    fourth commit

--- rectanglearea.py
+++ rectanglearea.py
@@ -1,5 +1,6 @@
 # program to find the area of rectangle
 # taking length and breadth from user
+# apply the formula of area
 len=int(input("enter a no:"))
 breadth=int(input("enter a number:"))
 area=len*breadth
