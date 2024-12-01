1e46e0e844e8ba39596fa3c78b13c46be558657f
Gurdeep <gurdeepdalal05@gmail.com>
2024-12-01 17:07:09 +0000

    fourth commit

--- twolists.py
+++ twolists.py
@@ -1,6 +1,6 @@
 #  Program to Convert Two Lists Into a Dictionary
 # using zip()
-# taking 2 different lists
+# taking two different lists
 l1=[1,2,3]
 l2=["hello","hii","python"]
 dict=dict(zip(l1,l2))
