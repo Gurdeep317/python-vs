bc3c6872ac84e4e5193dccb569ffb4c8d44d83e1
Gurdeep <gurdeepdalal05@gmail.com>
2024-12-02 17:46:31 +0000

    fifth commit

---
+++ sum1.py
@@ -0,0 +1,8 @@
+n = 50
+sum = sum(range(1, n + 1))
+print("The sum of the first 50 natural numbers is:", sum)
+print()
+
+
+total_sum = sum(range(1, 51))
+print("The sum of numbers from 1 to 50 is:", total_sum)
