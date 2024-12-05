50f909ac8b545c990dad0a4c9a28d94e409197e5
Gurdeep <gurdeepdalal05@gmail.com>
2024-12-05 17:20:00 +0000

    fifth commit

--- calender.py
+++ calender.py
@@ -1,8 +1,6 @@
 # program to make a calender means find date,month,year
 import calendar
-
 year=int(input("enter a year:"))
 month=int(input("enter a month:"))
-
 calendar = calendar.month(year,month)
 print(calendar)
