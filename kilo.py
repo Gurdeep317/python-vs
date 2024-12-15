6a0586e5d08d3d63f00229f2cfec0d0c990cd229
Gurdeep <gurdeepdalal05@gmail.com>
2024-12-15 16:33:23 +0000

    fifth commit

--- kilo.py
+++ kilo.py
@@ -1,5 +1,6 @@
 # 1 km = 0.621371mile
 # program to convert km to miles
+# taking km as input from user
 km=float(input("enter your value in km:"))
 miles=0.621371*km
 print(km,"km into miles will be:",miles,"miles")
