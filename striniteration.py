65473eec0d3db91a4e57bf4e9919cccce81c0dba
Gurdeep <gurdeepdalal05@gmail.com>
2025-01-21 17:39:19 +0000

    jan commit

--- striniteration.py
+++ striniteration.py
@@ -1,5 +1,16 @@
-a="welcome to our class"
-b=len(a)
+# a="welcometoourclass"
+# b=len(a)
+# print(b
+# for c in range(b):
+#     print(a[c])
+# print(a.find('n'))
+# print(a.index('n'))
+
+# print(a.isalpha())
+# b="1234"
+# print(b.isdigit())
+a=chr(65)
+print(a)
+print(type(a))
+b=ord('c')
 print(b)
-for c in range(b):
-    print(a[c])
