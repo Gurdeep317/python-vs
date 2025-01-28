9b8cb8c83362779a29c99d013ef7b749a141b960
your name <dalalgurdeep013@gmail.com>
2025-01-28 23:29:00 +0530

    jan commit

--- squareroot.py
+++ squareroot.py
@@ -2,10 +2,12 @@ num =89
 sr=num**(0.5)
 print("the square root of a number=",sr)
 print()
+
 num1=int(input("enter the value:"))
 sr=num1**(1/2)
 print("the square root of a given number=",sr)
 print()
+
 import math
 num2=int(input("enter the value:"))
 sr=math.sqrt(num2)
--- sum.py
+++ sum.py
@@ -1,6 +1,7 @@
 total_sum = 0
 print("Enter 50 numbers:")
 
+
 for i in range(50):
     number = float(input(f"Enter number {i+1}: "))
     total_sum += number
