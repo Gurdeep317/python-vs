007cf6f4c107f8b469be3b42048313d74f801781
your name <dalalgurdeep013@gmail.com>
2025-02-14 21:23:18 +0530

    feb commit

--- faceattendance.py/main.py
+++ faceattendance.py/main.py
@@ -6,14 +6,40 @@ class Face_Recognition_System:
         self.root=root
         self.root.geometry("1530x790+0+0")
         self.root.title("face recognition system")
-
-        img=Image.open(r"C:\Users\lenovo\OneDrive.jpg")
+        
+    # first image
+        img=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures.jpg")
         img=img.resize((500,130),Image.ANTIALIAS)
         self.photoimg=ImageTk.PhotoImage(img)
-
+    
         f_lbl=Label(self.root,image=self.photoimg)
         f_lbl.place(x=0,y=0,width=500,height=130)
 
+    # 2nd image
+        img1=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures.jpg")
+        img1=img1.resize((500,130),Image.ANTIALIAS)
+        self.photoimg1=ImageTk.PhotoImage(img1)
+
+        f_lbl=Label(self.root,image=self.photoimg1)
+        f_lbl.place(x=500,y=0,width=500,height=130)
+
+    # 3rd image
+        img2=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures.jpg")
+        img2=img2.resize((500,130),Image.ANTIALIAS)
+        self.photoimg2=ImageTk.PhotoImage(img2)
+
+        f_lbl=Label(self.root,image=self.photoimg2)
+        f_lbl.place(x=1000,y=0,width=550,height=130)
+
+    # background image
+        img3=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures.jpg")
+        img3=img2.resize((500,130),Image.ANTIALIAS)
+        self.photoimg3=ImageTk.PhotoImage(img3)
+
+        bg_img=Label(self.root,image=self.photoimg3)
+        bg_img.place(x=1000,y=0,width=550,height=130)
+
+    
     
 
 
@@ -24,7 +50,7 @@ class Face_Recognition_System:
 
 
 
-if __name__=="__main":
+if __name__=="__main__":
     root=Tk()
     obj=Face_Recognition_System(root)
     root.mainloop()
