9c43e523bef58e2a359502c9847c25d7ed849be8
your name <dalalgurdeep013@gmail.com>
2025-02-18 00:22:28 +0530

    feb commit

---
+++ faceattendance.py/student.py
@@ -0,0 +1,50 @@
+from tkinter import*
+from tkinter import ttk
+from PIL import Image,ImageTk
+class Student:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("face recognition system")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    root=Tk()
+    obj=Student(root)
+    root.mainloop()
