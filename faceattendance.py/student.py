12d1a08471da29fd8aade351c0c30bad461a59e3
Gurdeep <gurdeepdalal05@gmail.com>
2025-02-17 18:53:42 +0000

    feb commit

--- faceattendance.py/student.py
+++ faceattendance.py/student.py
@@ -7,6 +7,30 @@ class Student:
         self.root.geometry("1530x790+0+0")
         self.root.title("face recognition system")
 
+    # 1st image
+        img=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\university1.jpeg")
+        img=img.resize((500,130),Image.ANTIALIAS)
+        self.photoimg=ImageTk.PhotoImage(img)
+    
+        f_lbl=Label(self.root,image=self.photoimg)
+        f_lbl.place(x=0,y=0,width=500,height=130)
+
+    # 2nd image
+        img1=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\face recognition image.png")
+        img1=img1.resize((500,130),Image.ANTIALIAS)
+        self.photoimg1=ImageTk.PhotoImage(img1)
+
+        f_lbl=Label(self.root,image=self.photoimg1)
+        f_lbl.place(x=500,y=0,width=500,height=130)
+
+    # 3rd image
+        img2=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\university2.jpg")
+        img2=img2.resize((500,130),Image.ANTIALIAS)
+        self.photoimg2=ImageTk.PhotoImage(img2)
+
+        f_lbl=Label(self.root,image=self.photoimg2)
+        f_lbl.place(x=1000,y=0,width=550,height=130)
+
 
 
 
