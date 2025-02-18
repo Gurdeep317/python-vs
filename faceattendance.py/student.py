4af2bd4fe5111c4f6421d1713de25c6e842e58eb
Gurdeep <gurdeepdalal05@gmail.com>
2025-02-18 17:25:34 +0000

    feb commit

--- faceattendance.py/student.py
+++ faceattendance.py/student.py
@@ -45,6 +45,10 @@ class Student:
         main_frame=Frame(bg_img,bd=2)
         main_frame.place(x=5,y=55,width=1500,height=600)
 
+    # left label frame
+        left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("times new roman",12,"bold"))
+        left_frame.place(x=10,y=10,width=660,height=580)
+
 
 
 
