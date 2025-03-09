58192ac3ad7f27baf37e68b8ca9cf83c2dcabdee
your name <dalalgurdeep013@gmail.com>
2025-03-09 23:49:52 +0530

    march commit

--- faceattendance.py/main.py
+++ faceattendance.py/main.py
@@ -1,6 +1,11 @@
 from tkinter import*
 from tkinter import ttk
 from PIL import Image,ImageTk
+from student import Student
+
+
+
+
 class Face_Recognition_System:
     def __init__(self,root):
         self.root=root
@@ -9,7 +14,7 @@ class Face_Recognition_System:
 
     # 1st image
         img=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\university1.jpeg")
-        img=img.resize((500,130),Image.ANTIALIAS)
+        img=img.resize((500,130),Image.LANCZOS)
         self.photoimg=ImageTk.PhotoImage(img)
     
         f_lbl=Label(self.root,image=self.photoimg)
@@ -17,7 +22,7 @@ class Face_Recognition_System:
 
     # 2nd image
         img1=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\face recognition image.png")
-        img1=img1.resize((500,130),Image.ANTIALIAS)
+        img1=img1.resize((500,130),Image.LANCZOS)
         self.photoimg1=ImageTk.PhotoImage(img1)
 
         f_lbl=Label(self.root,image=self.photoimg1)
@@ -25,7 +30,7 @@ class Face_Recognition_System:
 
     # 3rd image
         img2=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\university2.jpg")
-        img2=img2.resize((500,130),Image.ANTIALIAS)
+        img2=img2.resize((500,130),Image.LANCZOS)
         self.photoimg2=ImageTk.PhotoImage(img2)
 
         f_lbl=Label(self.root,image=self.photoimg2)
@@ -33,7 +38,7 @@ class Face_Recognition_System:
 
     # background image
         img3=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\backgnd.png")
-        img3=img3.resize((1530,710),Image.ANTIALIAS)
+        img3=img3.resize((1530,710),Image.LANCZOS)
         self.photoimg3=ImageTk.PhotoImage(img3)
 
         bg_img=Label(self.root,image=self.photoimg3)
@@ -44,18 +49,18 @@ class Face_Recognition_System:
 
         # student buttton
         img4=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\student.jpeg")
-        img4=img4.resize((220,220),Image.ANTIALIAS)
+        img4=img4.resize((220,220),Image.LANCZOS)
         self.photoimg4=ImageTk.PhotoImage(img4)
 
-        b1=Button(bg_img,image=self.photoimg4,cursor="hand2")
+        b1=Button(bg_img,image=self.photoimg4,command=self.student_details,cursor="hand2")
         b1.place(x=200,y=100,width=220,height=220)
 
-        b1_1=Button(bg_img,text="STUDENT DETAILS",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1=Button(bg_img,text="STUDENT DETAILS",command=self.student_details,cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=200,y=300,width=220,height=40)
 
         # Detect face buttton
         img5=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\facedetector.jpeg")
-        img5=img5.resize((220,220),Image.ANTIALIAS)
+        img5=img5.resize((220,220),Image.LANCZOS)
         self.photoimg5=ImageTk.PhotoImage(img5)
 
         b1=Button(bg_img,image=self.photoimg5,cursor="hand2")
@@ -66,7 +71,7 @@ class Face_Recognition_System:
 
         # Attendance face buttton
         img6=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\attendance2.jpeg")
-        img6=img6.resize((220,220),Image.ANTIALIAS)
+        img6=img6.resize((220,220),Image.LANCZOS)
         self.photoimg6=ImageTk.PhotoImage(img6)
 
         b1=Button(bg_img,image=self.photoimg6,cursor="hand2")
@@ -77,7 +82,7 @@ class Face_Recognition_System:
 
         # Help face buttton
         img7=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\helpdesk.jpeg")
-        img7=img7.resize((220,220),Image.ANTIALIAS)
+        img7=img7.resize((220,220),Image.LANCZOS)
         self.photoimg7=ImageTk.PhotoImage(img7)
 
         b1=Button(bg_img,image=self.photoimg7,cursor="hand2")
@@ -88,7 +93,7 @@ class Face_Recognition_System:
 
         # Train face buttton
         img8=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\train.jpeg")
-        img8=img8.resize((220,220),Image.ANTIALIAS)
+        img8=img8.resize((220,220),Image.LANCZOS)
         self.photoimg8=ImageTk.PhotoImage(img8)
 
         b1=Button(bg_img,image=self.photoimg8,cursor="hand2")
@@ -99,7 +104,7 @@ class Face_Recognition_System:
 
         # Photos face buttton
         img9=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\photos.jpg")
-        img9=img9.resize((220,220),Image.ANTIALIAS)
+        img9=img9.resize((220,220),Image.LANCZOS)
         self.photoimg9=ImageTk.PhotoImage(img9)
 
         b1=Button(bg_img,image=self.photoimg9,cursor="hand2")
@@ -110,7 +115,7 @@ class Face_Recognition_System:
 
         # Developer face buttton
         img10=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\developer.jpg")
-        img10=img10.resize((220,220),Image.ANTIALIAS)
+        img10=img10.resize((220,220),Image.LANCZOS)
         self.photoimg10=ImageTk.PhotoImage(img10)
 
         b1=Button(bg_img,image=self.photoimg10,cursor="hand2")
@@ -121,7 +126,7 @@ class Face_Recognition_System:
 
         # Exit face buttton
         img11=Image.open(r"C:\Users\lenovo\OneDrive\attendanceproject pictures\exit.jpeg")
-        img11=img11.resize((220,220),Image.ANTIALIAS)
+        img11=img11.resize((220,220),Image.LANCZOS)
         self.photoimg11=ImageTk.PhotoImage(img11)
 
         b1=Button(bg_img,image=self.photoimg11,cursor="hand2")
@@ -129,6 +134,13 @@ class Face_Recognition_System:
 
         b1_1=Button(bg_img,text="Exit",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=1100,y=580,width=220,height=40)
+
+
+
+# =============================functions buttons==================================
+    def student_details(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Student(self.new_window)
 
 
 
