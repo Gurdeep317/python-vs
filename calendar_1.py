09c475e6d3bed6837a81176fc10c42a8bd03ee6f
your name <dalalgurdeep013@gmail.com>
2025-03-20 22:53:11 +0530

    march commit

--- calendar_1.py
+++ calendar_1.py
@@ -1,10 +1,7 @@
 # program to make a calender means find date,month,year
 # import calendar
-
 import calendar
-
 year=int(input("enter a year:"))
 month=int(input("enter a month:"))
-
 calendar = calendar.month(year,month)
 print(calendar)
