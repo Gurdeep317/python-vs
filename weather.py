853a623a1cfaea19fcc4cae40b366c23dbd35392
Gurdeep <gurdeepdalal05@gmail.com>
2025-03-25 18:51:46 +0000

    march commit

--- weather.py
+++ weather.py
@@ -1,6 +1,9 @@
 import requests
-
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+BASE_URL="https://api.openweathermap.org/data/2.5/weather"
 
 
 def fetch_weather(data):
