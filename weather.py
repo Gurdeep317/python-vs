615a59dc712e7201c9061daff6968e59994b06bc
Gurdeep <gurdeepdalal05@gmail.com>
2025-03-25 19:02:16 +0000

    march commit

--- weather.py
+++ weather.py
@@ -6,8 +6,11 @@ API_KEY = os.getenv("API_KEY")
 BASE_URL="https://api.openweathermap.org/data/2.5/weather"
 
 
-def fetch_weather(data):
-    params={"q":data,"appid":API_KEY,"units":"metric"}
+def fetch_weather(city):
+    if not API_KEY:
+        return {"error": "API key not found. Check your .env file."}
+    
+    params={"q":city,"appid":API_KEY,"units":"metric"}
 
     try:
         response = requests.get(BASE_URL, params=params)
@@ -17,28 +20,28 @@ def fetch_weather(data):
         return {"error": str(e)}
 
 
-def display_weather(city):
+def display_weather(data):
 
-    if "error" in city:
-        print(f"Error:{city['error']}")
+    if "error" in data:
+        print(f"Error:{data['error']}")
         return
     
-    if city.get("cod")!=200:
-        print(f"Error:{city.get('message','Invalid city')}")
+    if data.get("cod")!=200:
+        print(f"Error:{data.get('message','Invalid city')}")
         return
     
 
-    print(f"\nWeather in {city['name']}:")
-    print(f" Temperature: {city['main']['temp']}°C")
-    print(f" Condition: {city['weather'][0]['description'].title()}")
-    print(f" Wind Speed: {city['wind']['speed']} m/s")
-    print(f" Humidity: {city['main']['humidity']}%\n")
+    print(f"\nWeather in {data['name']}:")
+    print(f" Temperature: {data['main']['temp']}°C")
+    print(f" Condition: {data['weather'][0]['description'].title()}")
+    print(f" Wind Speed: {data['wind']['speed']} m/s")
+    print(f" Humidity: {data['main']['humidity']}%\n")
 
 
-    if __name__=="__main__":
-        cityname=input("enter city name:")
-        weather_data= fetch_weather(city)
-        display_weather(weather_data)
+if __name__=="__main__":
+    cityname=input("enter city name:")
+    weather_data= fetch_weather(cityname)
+    display_weather(weather_data)
 
 
     
